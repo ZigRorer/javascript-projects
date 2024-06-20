4911d4d227524cd45bafd399938c0948a6b02f49
Admin <admin@Admins-MacBook-Air.local>
2024-06-20 17:15:56 -0400

    Examples and Exercises updated

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,5 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+let phrase = "JavaScript rocks!";
+console.log(phrase[phrase.length - 8]);
--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "candles";
+let name = "Axel";
+let verb = "play";
+let adjective = "delicious";
+let color = "purple";
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
