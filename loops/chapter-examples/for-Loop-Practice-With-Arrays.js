49bf31acfcea9b979de05f9ed9cdc6429c6f56a1
Admin <admin@Admins-MacBook-Air.local>
2024-06-24 09:11:06 -0400

    Chapter Exercises updated

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,6 @@
 // create an array variable containing the names
-
+let honeys = ["Ziggie", "Axel", "Mambo", "Obsidian"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < honeys.length; i++) {
+    console.log(honeys[i]);
+}
