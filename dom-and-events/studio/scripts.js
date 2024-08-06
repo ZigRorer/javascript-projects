a08863f0b879b1a103880437cb05a02985158747
Admin <admin@Admins-MacBook-Air.local>
2024-08-05 21:33:04 -0400

    Studio updated

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,25 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function() {
+    let takeoff = this.document.getElementById("takeoff");
+    let flightStatus = this.document.getElementById("flightStatus");
+    let shuttleBackground = this.document.getElementById("shuttleBackground");
+    let spaceShuttleHeight = this.document.getElementById("spaceShuttleHeight");
+    let landing = this.document.getElementById("landing");
+
+    takeoff.addEventListener("click", function () {
+        let confirmed = window.confirm("Confirm that the shuttle is ready for takeoff.");
+
+        if (confirmed) {
+           console.log("This is better.");
+           flightStatus.innerHTML = "Shutte in flight.";
+           shuttleBackground.style.backgroundColor = "blue";
+           spaceShuttleHeight.innerHTML ++;
+        }
+    });
+
+    landing.addEventListener("click", function(){
+        let confirmed = window.confirm("The shuttle is landing. LAnding gear engaged")
+    })
+});
