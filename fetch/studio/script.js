61d273018efa0eb5f7a306a1048fbc3aadf8da59
Admin <admin@Admins-MacBook-Air.local>
2024-08-12 15:08:03 -0400

    Studio updated

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,26 @@
-//TODO: Add Your Code Below
+window.addEventListener('load', function() {
+    const container = document.getElementById('container')
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function (response){
+        response.json().then(function (data) {
+            console.log(data);
+            
+            for (let i=0; i < data.length; i++) {
+                let astronaut = data[i];
+                let activeClass = astronaut.active ? "active" : "";
+                container.innerHTML += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours In Space: ${astronaut.hoursInSpace}</li>
+                            <li class="${activeClass}">Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills.join(", ")}</li>
+                        </ul>
+                    </div>
+                    <img class="avatar" src="${astronaut.picture}">
+                </div>
+                `;
+            }
+        })
+    });
+});
